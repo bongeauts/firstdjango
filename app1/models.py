c354d3b6494cfeff1c538e33641595bcefc4eb3e
bongeauts <bergonzi.luca92@gmail.com>
2018-07-25 07:26:50 -0700

    first web freecad

--- app1/models.py
+++ app1/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Box(models.Model):
-    id = pk
     boxLength = models.FloatField()
     boxHeight = models.FloatField()
     boxWidth = models.FloatField()
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,8 @@
-import sys
-sys.path.append(0,'/user(lib/freecad/lib/')
+
 import FreeCAD
+import Part
+import importWebGL
+
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
@@ -9,6 +11,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Box
 from .serializers import BoxSerializer
+from django.template import loader
 
 def index(request):
     return HttpResponse("<h1>this is app1 homepage")
@@ -24,12 +27,36 @@ class BoxList(APIView):
     def post(self):
         pass
 
-class CreatePart(APIView):
+def cube(request):
+    template = loader.get_template('cube.html')
+    context ={}
+    return HttpResponse(template.render(context,request))
 
-    def get(self, request, pk):
-        box_pk = Box.objects.get(pk=pk)
-        doc = FreeCAD.newDocument()
-        box_fc = doc.addObject("Part::Box","mybox")
-        box_fc.Height = box_pk.boxHeight
-        box_fc.Width = box_pk.boxWidth
-        box_fc.Length = box_pk.boxLength
+def createCube(request, pk):
+    box_pk = Box.objects.get(pk=pk)
+    doc = FreeCAD.newDocument("Unnamed1")
+    doc.addObject("Part::Box", "Box")
+    doc.ActiveObject.Label = "Cube"
+    doc.recompute()
+    FreeCAD.getDocument("Unnamed1").getObject("Box").Length = box_pk.boxLength
+    FreeCAD.getDocument("Unnamed1").getObject("Box").Width = box_pk.boxWidth
+    FreeCAD.getDocument("Unnamed1").getObject("Box").Height = box_pk.boxHeight
+    doc.recompute()
+    __objs__ = []
+    __objs__.append(FreeCAD.getDocument("Unnamed1").getObject("Box"))
+    importWebGL.export(__objs__, u"/home/luca/djangovenv2/website/app1/templates/cube1.html")
+    template = loader.get_template('cube1.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+# class CreatePart(APIView):
+#
+#     def get(self, request, pk):
+#         box_pk = Box.objects.get(pk=pk)
+#         doc = FreeCAD.newDocument()
+#         box_fc = doc.addObject("Part::Box","mybox")
+#         box_fc.Height = box_pk.boxHeight
+#         box_fc.Width = box_pk.boxWidth
+#         box_fc.Length = box_pk.boxLength
+#         doc.recompute()
+#         FreeCAD.
